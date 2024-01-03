02ac0ee48d7ba1bebb88592c927cf5cb4763dd7e
islam solaiman <islam.solaiman92@gmail.com>
2024-01-04 03:46:12 +0400

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd0c6a4aa-d368-4f01-b7ae-992ce4af2cc6';
+export const MODAL_CLOSE_SEC = 2.5;
